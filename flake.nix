685f36d920f3b502d8dd3d06003c836b03bfda3c
Logan Devine <logan@zirco.dev>
2026-05-09 10:00:50 -0700

    add nix flake, update dependencies

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "tweaker";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      naersk,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust = (
+          pkgs.rust-bin.nightly.latest.default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+            ];
+          }
+        );
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust
+            pkg-config
+            openssl
+            nixfmt
+          ];
+        };
+
+        packages.tweaker =
+          let
+            naerskLib = naersk.lib.${system}.override {
+              cargo = rust;
+              rustc = rust;
+            };
+          in
+          naerskLib.buildPackage {
+            pname = "zrc";
+            src = ./.;
+            buildInputs = with pkgs; [
+              rust
+              openssl
+              pkg-config
+            ];
+          };
+
+        packages.default = self.packages.${system}.tweaker;
+      }
+    );
+}
